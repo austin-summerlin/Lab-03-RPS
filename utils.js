250399408b0fa9ee643090263fd4569673860534
Austin Summerlin <summerlin.austin@gmail.com>
2021-03-25 17:55:08 -0700

    tweaked appearance

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ export function didUserWin(user, computer) {
     if (user === 'rock' && computer === 'scissors') return 'win';
     if (user === 'rock' && computer === 'paper') return 'lose';
     if (user === 'rock' && computer === 'rock') return 'tie';
-    if (user === 'paper' && computer === 'scissors') return 'los';
+    if (user === 'paper' && computer === 'scissors') return 'lose';
     if (user === 'paper' && computer === 'paper') return 'tie';
     if (user === 'paper' && computer === 'rock') return 'win';
     if (user === 'scissors' && computer === 'scissors') return 'tie';
